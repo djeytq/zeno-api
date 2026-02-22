21fb4ff84f09616ce2d051d9aa955fd0ca6b45ba
djeytq <djeytiquimica@gmail.com>
2026-02-22 09:35:57 +0100

    criei as rotas do crud do cadstro e tirei o .env do versionamento

--- src/Routes/cadastros.routes.js
+++ src/Routes/cadastros.routes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 const CadastrosController = require('../Controller/cadastros');
 
-router.get('')
+router.post('/register', CadastrosController.Register);
+router.get('/show', CadastrosController.Show);
+router.put('/update', CadastrosController.Update);
+router.delete('/delete/:id', CadastrosController.Delete);
+
+module.exports = router;
